c48eae7e29c87dc4f465f8fbf4e97d26237d5eb9
Paulo Vieira <paulovieira@MacBook-Pro-de-Paulo.local>
2017-10-31 16:08:14 +0000

    Bootstrap 4
    -Add Bootstrap 4
    -Add Navaba&Footer
    -Add Backgroud Img
    -Starting Homepage

---
+++ laravel/resources/views/header.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>MemoryGame</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link	href="{{ URL::asset('css/style.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+
+      <nav class="navbar fixed-top navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">BadMemoryGame</a>
+        <form class="form-inline my-2 my-lg-0">
+          <li>
+            <button class="btn btn-outline-success" type="submit">Login</button>
+            <button class="btn btn-outline-success" type="submit">Registar</button>
+          </li>
+        </form>
+      </nav>
